18bb6ea5df6bd197e624a7da1f79b649084f0bd3
Angelo Manlangit <manlangitangelomh@gmail.com>
2026-01-31 01:21:04 +0800

    Cool main menu

--- scripts/UI/main_menu.gd
+++ scripts/UI/main_menu.gd
@@ -1,12 +1,34 @@
 extends Control
 
-@onready var video_stream_player: VideoStreamPlayer = $VideoStreamPlayer
 @onready var loading: Control = $Loading
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	video_stream_player.hide()
-	pass # Replace with function body.
+@onready var boomerang: Node3D = $SubViewportContainer/SubViewport/boomerang
+@onready var handfan: Node3D = $SubViewportContainer/SubViewport/handfan
+@onready var sticky_hand: Node3D = $"SubViewportContainer/SubViewport/sticky hand"
+
+var rng = RandomNumberGenerator.new()
+
+var my_stylebox = load("res://scenes/main_menu.tscn::StyleBoxFlat_l6cm7") as StyleBoxFlat
+
+func _ready():
+	rng.randomize()
+	
+	boomerang.visible = false
+	handfan.visible = false
+	sticky_hand.visible = false
+	
+	var my_random_number = rng.randi_range(1, 3)
+	
+	match my_random_number:
+		1:
+			boomerang.visible = true
+			my_stylebox.bg_color = Color.from_rgba8(60, 105, 139, 255)
+		2:
+			handfan.visible = true
+			my_stylebox.bg_color = Color.from_rgba8(175, 25, 0, 255)
+		3:
+			sticky_hand.visible = true
+			my_stylebox.bg_color = Color.from_rgba8(58, 121, 42, 255)
 
 func _on_start_pressed() -> void:
 	print("Start Pressed")
@@ -14,8 +36,6 @@ func _on_start_pressed() -> void:
 
 func _on_options_pressed() -> void:
 	print("Options Pressed")
-	video_stream_player.show()
-	video_stream_player.play()
 
 func _on_exit_pressed() -> void:
 	print("Exit Pressed")
